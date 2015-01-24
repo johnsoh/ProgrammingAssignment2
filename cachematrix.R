f749051887f153ebbbd8e6e42b2061fb9f3f14ff
johnsoh <johnsoh.01@gmail.com>
2015-01-24 17:17:09 +0800

    pushing inverse function

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-# commit once
-
+## Returns a list containing functions that are able to cache inverse matrix value
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedValue <- NULL
+  
+  # Creating functions that point to the same environment
+  getCache <- function() { cachedValue }
+  getOriginal <- function() { x }
+  setCache <- function(newVal) { cachedValue <<- newVal }
+  
+  # Returning the functions in a list
+  list(getCache = getCache, getOriginal = getOriginal, setCache = setCache)
 }
 
-
-## Write a short comment describing this function
-
+## If cache is empty, fills it with inverse. Returns the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if (is.null(x$getCache())) 
+  {
+    temp <- solve(x$getOriginal())
+    x$setCache(temp)
+  }
+  ## Return a matrix that is the inverse of 'x'
+  x$getCache()
 }
+
+
+
